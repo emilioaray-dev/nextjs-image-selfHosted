3dcd3795c7c96ade5c4e0565d3e6b8f6b041efd5
Celsius Aray <emilioaray@gmail.com>
2025-10-28 21:21:31 -0400

    feat: configurar opciones de imágenes en next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    qualities: [25, 50, 75, 100],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+
+  },
 };
 
 export default nextConfig;
